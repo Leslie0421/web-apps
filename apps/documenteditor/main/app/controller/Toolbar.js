aabe5cdcb73bec62bba6e2e3fae01e1ba270771f
jnbcnb <563374418@qq.com>
2024-06-26 17:08:30 +0800

    feat: 新增在本页及之后插入页码逻辑

--- apps/documenteditor/main/app/controller/Toolbar.js
+++ apps/documenteditor/main/app/controller/Toolbar.js
@@ -74,6 +74,7 @@ define([
         ],
 
         initialize: function() {
+            sessionStorage.removeItem('afterIndex')
             this._state = {
                 activated: false,
                 bullets: {
@@ -372,6 +373,7 @@ define([
             toolbar.btnEditHeader.menu.on('item:click',                 _.bind(this.onEditHeaderFooterClick, this));
             toolbar.btnInsDateTime.on('click',                          _.bind(this.onInsDateTimeClick, this));
             toolbar.mnuPageNumCurrentPos.on('click',                    _.bind(this.onPageNumCurrentPosClick, this));
+            toolbar.mnuPageNumAfterPos.on('click',                    _.bind(this.onPageNumAfterPosClick, this));
             toolbar.mnuInsertPageCount.on('click',                      _.bind(this.onInsertPageCountClick, this));
             toolbar.btnBlankPage.on('click',                            _.bind(this.onBtnBlankPageClick, this));
             toolbar.listStyles.on('click',                              _.bind(this.onListStyleSelect, this));
@@ -849,7 +851,7 @@ define([
                 toolbar.mnuDropCapAdvanced.setDisabled(disable_dropcapadv);
 
             this.toolbar.lockToolbar(Common.enumLock.cantAddTable, !can_add_table, {array: [toolbar.btnInsertTable]});
-            this.toolbar.lockToolbar(Common.enumLock.cantAddPageNum, toolbar.mnuPageNumCurrentPos.isDisabled() && toolbar.mnuPageNumberPosPicker.isDisabled(), {array: [toolbar.mnuInsertPageNum]});
+            this.toolbar.lockToolbar(Common.enumLock.cantAddPageNum, toolbar.mnuPageNumCurrentPos.isDisabled() && toolbar.mnuPageNumAfterPos.isDisabled() && toolbar.mnuPageNumberPosPicker.isDisabled(), {array: [toolbar.mnuInsertPageNum]});
             this.toolbar.lockToolbar(Common.enumLock.inHeader, in_header, {array: toolbar.btnsPageBreak.concat([toolbar.btnBlankPage])});
             this.toolbar.lockToolbar(Common.enumLock.inControl, in_control, {array: toolbar.btnsPageBreak.concat([toolbar.btnBlankPage])});
             this.toolbar.lockToolbar(Common.enumLock.cantPageBreak, in_image && !btn_eq_state, {array: toolbar.btnsPageBreak.concat([toolbar.btnBlankPage])});
@@ -959,7 +961,7 @@ define([
 
         onApiLockHeaderFooters: function() {
             this.toolbar.lockToolbar(Common.enumLock.headerFooterLock, true, {array: [this.toolbar.mnuPageNumberPosPicker]});
-            this.toolbar.lockToolbar(Common.enumLock.cantAddPageNum, this.toolbar.mnuPageNumCurrentPos.isDisabled(), {array: [this.toolbar.mnuInsertPageNum]});
+            this.toolbar.lockToolbar(Common.enumLock.cantAddPageNum, this.toolbar.mnuPageNumCurrentPos.isDisabled() && this.toolbar.mnuPageNumAfterPos.isDisabled(), {array: [this.toolbar.mnuInsertPageNum]});
         },
 
         onApiUnLockHeaderFooters: function() {
@@ -2424,6 +2426,21 @@ define([
 
             if (e.type !== 'click')
                 this.toolbar.btnEditHeader.menu.hide();
+
+            Common.NotificationCenter.trigger('edit:complete', this.toolbar);
+            Common.component.Analytics.trackEvent('ToolBar', 'Page Number');
+        },
+
+        onPageNumAfterPosClick: function(item, e) {
+            const pageIndex = this.api.getCurrentPage();
+            sessionStorage.setItem('afterIndex',pageIndex)
+
+            if (this.api)
+                this.api.put_PageNum(-1);
+
+            if (e.type !== 'click')
+                this.toolbar.btnEditHeader.menu.hide();
+
             Common.NotificationCenter.trigger('edit:complete', this.toolbar);
             Common.component.Analytics.trackEvent('ToolBar', 'Page Number');
         },
--- apps/documenteditor/main/app/view/Toolbar.js
+++ apps/documenteditor/main/app/view/Toolbar.js
@@ -739,8 +739,15 @@ define([
                         options: {}
                     };
                     this.mnuPageNumCurrentPos = clone(this.mnuPageNumberPosPicker);
+                    this.mnuPageNumAfterPos = clone(this.mnuPageNumberPosPicker);
+                    
                     this.mnuPageNumCurrentPos.options.lock = [_set.paragraphLock, _set.headerLock, _set.richEditLock, _set.plainEditLock];
+
+                    this.mnuPageNumAfterPos.options.lock = [_set.paragraphLock, _set.headerLock, _set.richEditLock, _set.plainEditLock];
+
                     this.paragraphControls.push(this.mnuPageNumCurrentPos);
+                    this.paragraphControls.push(this.mnuPageNumAfterPos);
+
                     this.mnuInsertPageCount = clone(this.mnuPageNumberPosPicker);
                     this.mnuInsertPageCount.options.lock = [_set.paragraphLock, _set.headerLock, _set.richEditLock, _set.plainEditLock];
                     this.paragraphControls.push(this.mnuInsertPageCount);
@@ -2277,8 +2284,10 @@ define([
                 );
 
                 var keepStateCurr = this.mnuPageNumCurrentPos.keepState,
+                    keepStateAfter= this.mnuPageNumAfterPos.keepState,
                     keepStateCount = this.mnuInsertPageCount.keepState,
                     keepStateNum = this.mnuInsertPageNum.keepState;
+
                 this.btnEditHeader.setMenu(
                     new Common.UI.Menu({
                         items: [
@@ -2303,6 +2312,12 @@ define([
                                             lock: this.mnuPageNumCurrentPos.options.lock,
                                             disabled: this.mnuPageNumCurrentPos.isDisabled(),
                                             value: 'current'
+                                        }),
+                                        this.mnuPageNumAfterPos = new Common.UI.MenuItem({
+                                            caption: '本页及之后',
+                                            lock: this.mnuPageNumAfterPos.options.lock,
+                                            disabled: this.mnuPageNumAfterPos.isDisabled(),
+                                            value: 'after'
                                         })
                                     ]
                                 })
@@ -2316,9 +2331,16 @@ define([
                     })
                 );
                 this.mnuInsertPageNum.keepState = keepStateNum;
+
                 this.mnuPageNumCurrentPos.keepState = keepStateCurr;
+                this.mnuPageNumAfterPos.keepState = keepStateAfter;
+
                 this.paragraphControls.push(this.mnuPageNumCurrentPos);
+                this.paragraphControls.push(this.mnuPageNumAfterPos);
+
                 this.lockControls.push(this.mnuPageNumCurrentPos);
+                this.lockControls.push(this.mnuPageNumAfterPos);
+
                 this.mnuInsertPageCount.keepState = keepStateCount;
                 this.paragraphControls.push(this.mnuInsertPageCount);
                 this.lockControls.push(this.mnuInsertPageCount);
